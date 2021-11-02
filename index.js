f5d950f4f5e5c5221b26ace81d88d30c058b8282
Edris_Azimi <Edris.azimi.pro@gmail.com>
2021-11-02 14:40:55 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+
+// Test 1
+document.getElementById('main').remove();
+
+// Test 2 and 3
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+
+// Test 4
+newHeader.innerHTML = "YOUR-NAME is the champion"
+
